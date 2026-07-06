4e332b17c9cb8aa9c818ecdde4c05cfe6a217c1e
Hari Prasad <hariprasad.kunde@gmail.com>
2026-07-06 22:07:02 +0530

    code refactor

--- fixtures/hooksFixture.ts
+++ fixtures/hooksFixture.ts
@@ -1,12 +1,25 @@
 import {test as baseTest} from '../fixtures/commonFixture';
+import { LoginPage } from '../pages/LoginPage';
+import { UserPage } from '../pages/UserPage';
+
 
 export const test = baseTest.extend<any>({
-    beforeEachHookFixture: async({loginPageFixture}, use)=>{
+    beforeEachHookFixture: async(
+        {loginPageFixture} : {loginPageFixture: LoginPage},
+        use: () => Promise<void>
+    )=>{
         await loginPageFixture.launchApplication();
         await use();
     },
-    afterEachHookFixture: async({userPageFixture}, use)=>{
+    afterEachHookFixture: async(
+        {userPageFixture} : {userPageFixture: UserPage},
+        use: ()=>Promise<void>
+    )=>{
         await use();
         await userPageFixture.logout();
     }
 })
+
+export {expect} from '@playwright/test';
+
+//https://chatgpt.com/s/t_6a4b768e035c8191b8734994a30bb709
