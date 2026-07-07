f94c7c4ef1d023dd93646917aa26b984b32510c6
Hari Prasad <hariprasad.kunde@gmail.com>
2026-07-07 12:36:05 +0530

    integrate test step

--- tests/pimPageTest.spec.ts
+++ tests/pimPageTest.spec.ts
@@ -8,7 +8,13 @@ test('[PIM] Add and verify a new employee in the PIM module', {
         description: 'PIM'
     }
 }, async({beforeEachHookFixture, navigationPanelPageFixture, pimPageFixture})=>{
-    await navigationPanelPageFixture.openPIMTab();
-    await pimPageFixture.addEmployee(pimData.firstName, pimData.middleName, pimData.lastName);
-    await expect(pimPageFixture.newEmployeeNameHeading).toHaveText(`${pimData.firstName} ${pimData.lastName}`);
-})
+    await test.step('Open PIM Module', async()=>{
+        await navigationPanelPageFixture.openPIMTab();
+    });
+    await test.step('Add a new employee', async()=>{
+        await pimPageFixture.addEmployee(pimData.firstName, pimData.middleName, pimData.lastName);
+    });
+    await test.step('Verify newly added employee name', async()=>{
+        await expect(pimPageFixture.newEmployeeNameHeading).toHaveText(`${pimData.firstName} ${pimData.lastName}`);
+    });
+});
