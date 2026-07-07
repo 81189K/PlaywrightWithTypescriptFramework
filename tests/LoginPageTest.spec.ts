948ebeb207bfb46fe780964d5d4cd8b433bcbe01
Hari Prasad <hariprasad.kunde@gmail.com>
2026-07-07 12:10:50 +0530

    Integrate Tags and Annotations

--- tests/LoginPageTest.spec.ts
+++ tests/LoginPageTest.spec.ts
@@ -13,20 +13,40 @@ test.use({
 
 
 /*** Read data from json file */
-test('Login with invalid password', async({loginPageFixture, commonUtilsFixture})=>{
+test('[Login] Verify login with an invalid password', {
+    tag: ['@UI', '@SMOKE' ],
+    annotation: {
+        // type: 'Test Case Link',
+        // description: 'JIRA_LINK'
+        type: 'Module',
+        description: 'Login'
+    }
+}, async({loginPageFixture, commonUtilsFixture})=>{
     const username = commonUtilsFixture.decrypt(process.env.USER_NAME!);
     await loginPageFixture.launchApplication(); //instead of beforeEachHookFixture fixture, directly calling launchApplication() using loginPageFixture
     await loginPageFixture.login(username, loginData.incorrectPassword);
     await expect(loginPageFixture.invalidLoginErrorPopup).toHaveText(loginData.invalidLoginErrorText);
 })
 
-test('Login with invalid username', async({beforeEachHookFixture, loginPageFixture, commonUtilsFixture})=>{
+test('[Login] Verify login with an invalid username', {
+    tag: ['@UI', '@SMOKE' ],
+    annotation: {
+        type: 'Module',
+        description: 'Login'
+    }
+}, async({beforeEachHookFixture, loginPageFixture, commonUtilsFixture})=>{
     const password = commonUtilsFixture.decrypt(process.env.PASSWORD!);
     await loginPageFixture.login(loginData.incorrectUsername, password);
     await expect(loginPageFixture.invalidLoginErrorPopup).toHaveText(loginData.invalidLoginErrorText);
 })
 
-test('Login with both invalid username and password', async({beforeEachHookFixture, loginPageFixture, })=>{
+test('[Login] Verify login with an invalid username and password', {
+    tag: ['@UI', '@SANITY' ],
+    annotation: {
+        type: 'Module',
+        description: 'Login'
+    }
+}, async({beforeEachHookFixture, loginPageFixture, })=>{
     await loginPageFixture.login(loginData.incorrectUsername, loginData.incorrectPassword);
     await expect(loginPageFixture.invalidLoginErrorPopup).toHaveText(loginData.invalidLoginErrorText);
 })
--- tests/pimPageTest.spec.ts
+++ tests/pimPageTest.spec.ts
@@ -1,7 +1,13 @@
 import {test, expect} from '../fixtures/hooksFixture';
 import pimData from '../testdata/pimTestData.json';
 
-test('[PIM]Add and Verify a new Employee in PIM module', async({beforeEachHookFixture, navigationPanelPageFixture, pimPageFixture})=>{
+test('[PIM] Add and verify a new employee in the PIM module', {
+    tag: ['@UI', '@SMOKE' ],
+    annotation: {
+        type: 'Module',
+        description: 'PIM'
+    }
+}, async({beforeEachHookFixture, navigationPanelPageFixture, pimPageFixture})=>{
     await navigationPanelPageFixture.openPIMTab();
     await pimPageFixture.addEmployee(pimData.firstName, pimData.middleName, pimData.lastName);
     await expect(pimPageFixture.newEmployeeNameHeading).toHaveText(`${pimData.firstName} ${pimData.lastName}`);
