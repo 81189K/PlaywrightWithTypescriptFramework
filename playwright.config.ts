1dcd338362392c6dce521d745fc96588094840d5
Hari Prasad <hariprasad.kunde@gmail.com>
2026-07-08 04:37:25 +0530

    handle multiple envs for API testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: 'https://restful-booker.herokuapp.com',
+    baseURL: process.env.API_BASE_URL,
     extraHTTPHeaders:{
       Accept: 'application/json', //avoid adding headers here, as it may cause errors in UI tests execution
     },
