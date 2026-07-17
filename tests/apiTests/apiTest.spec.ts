20c81773fbcfe3d3b8c4e10de96da4ad346f4031
Hari Prasad <hariprasad.kunde@gmail.com>
2026-07-17 12:46:32 +0530

    Delete API

--- tests/apiTests/apiTest.spec.ts
+++ tests/apiTests/apiTest.spec.ts
@@ -14,6 +14,8 @@ import apiTestData from '../../testdata/apiTestData.json';
 //run cmd: npx playwright test --project=apiTest
 // npm run test_demo_api
 
+let bookingID: string;
+let createdBookingID: string;
 test('Verify that the user is able to fetch all the booking IDs using GET API', {
     tag: ['@API', '@SMOKE'],
     annotation: {
@@ -21,17 +23,19 @@ test('Verify that the user is able to fetch all the booking IDs using GET API', 
         description: "GET"
     }
 }, async ({ request }) => {
-    const bookingIDsResponse = await request.get(apiTestData.GET_ALL.request.resource);
+    const bookingIDsResponse = await request.get(apiTestData.GET.request.resource);
     const bookingIDsJsonResponse = await bookingIDsResponse.json();
     console.log(bookingIDsJsonResponse);
     //verify response code/text
-    expect(bookingIDsResponse.status()).toBe(apiTestData.GET_ALL.response.statusCode);
-    expect(bookingIDsResponse.statusText()).toBe(apiTestData.GET_ALL.response.statusText);
+    expect(bookingIDsResponse.status()).toBe(apiTestData.GET.response.statusCode);
+    expect(bookingIDsResponse.statusText()).toBe(apiTestData.GET.response.statusText);
     expect(bookingIDsResponse.ok()).toBeTruthy();
     //verify response header
-    expect(bookingIDsResponse.headers()['content-type']).toBe(apiTestData.GET_ALL.response.headerContentType);
+    expect(bookingIDsResponse.headers()['content-type']).toBe(apiTestData.GET.response.headerContentType);
     //verify response body is not empty
     expect(bookingIDsJsonResponse).not.toBeNull();
+    bookingID = bookingIDsJsonResponse[1].bookingid;
+    console.log(`Booking ID: ${bookingID}`);
 });
 
 test('Verify that the user is able to fetch the booking details for a given ID using GET API', {
@@ -41,12 +45,12 @@ test('Verify that the user is able to fetch the booking details for a given ID u
         description: "GET"
     }
 }, async ({ request }) => {
-    const bookingResponse = await request.get(`${apiTestData.GET_ID.request.resource}/${apiTestData.GET_ID.request.ID}`);
+    const bookingResponse = await request.get(`${apiTestData.GET.request.resource}/${bookingID}`);
     const bookingJsonResponse = await bookingResponse.json();
     console.log(bookingJsonResponse);
     //verify response code/text
-    expect(bookingResponse.status()).toBe(apiTestData.GET_ID.response.statusCode);
-    expect(bookingResponse.statusText()).toBe(apiTestData.GET_ID.response.statusText);
+    expect(bookingResponse.status()).toBe(apiTestData.GET.response.statusCode);
+    expect(bookingResponse.statusText()).toBe(apiTestData.GET.response.statusText);
     expect(bookingResponse.ok()).toBeTruthy();
 
     //verify response body is not empty
@@ -63,7 +67,6 @@ test('Verify that the user is able to create new booking using POST API', {
     const createBookingResponse = await request.post(apiTestData.createBooking.request.resource, {
         data: apiTestData.createBooking.request.payload,
         headers: apiTestData.createBooking.request.headers
-
     });
     const bookingJsonResponse = await createBookingResponse.json();
     console.log(bookingJsonResponse);
@@ -72,6 +75,8 @@ test('Verify that the user is able to create new booking using POST API', {
 
     //verify response body
     expect(bookingJsonResponse.booking).toMatchObject(apiTestData.createBooking.request.payload);
+    createdBookingID = bookingJsonResponse.bookingid;
+    console.log(`Created booking ID: ${createdBookingID}`);
 });
 
 test('Verify that the user is able to update an existing booking using PUT API: using API Key', {
@@ -81,7 +86,7 @@ test('Verify that the user is able to update an existing booking using PUT API: 
         description: "PUT call using API Key"
     }
 }, async ({ request }) => {
-    const updateBookingResponse = await request.put(apiTestData.updateBooking.request.resource, {
+    const updateBookingResponse = await request.put(`${apiTestData.updateBooking.request.resource}/${createdBookingID}`, {
         data: apiTestData.updateBooking.request.payload,
         headers: apiTestData.updateBooking.request.headers
 
@@ -107,7 +112,7 @@ test('Verify that the user is able to update an existing booking using PUT API: 
     //update token value in apiTestData
     apiTestData.updateBooking2.request.headers.Cookie = apiTestData.updateBooking2.request.headers.Cookie.replace("{{authToken}}", authToken)
     
-    const updateBookingResponse = await request.put(apiTestData.updateBooking2.request.resource, {
+    const updateBookingResponse = await request.put(`${apiTestData.updateBooking2.request.resource}/${createdBookingID}`, {
         data: apiTestData.updateBooking2.request.payload,
         headers: apiTestData.updateBooking2.request.headers
     });
@@ -132,7 +137,7 @@ test('Verify that the user is able to update an existing booking using PATCH API
     //update token value in apiTestData
     apiTestData.patchBooking.request.headers.Cookie = apiTestData.patchBooking.request.headers.Cookie.replace("{{authToken}}", authToken)
     
-    const patchBookingResponse = await request.patch(apiTestData.patchBooking.request.resource, {
+    const patchBookingResponse = await request.patch(`${apiTestData.patchBooking.request.resource}/${createdBookingID}`, {
         data: apiTestData.patchBooking.request.payload,
         headers: apiTestData.patchBooking.request.headers
     });
@@ -144,4 +149,31 @@ test('Verify that the user is able to update an existing booking using PATCH API
     //verify response body
     expect(bookingJsonResponse.firstname).toMatch(apiTestData.patchBooking.request.payload.firstname);
     expect(bookingJsonResponse.lastname).toMatch(apiTestData.patchBooking.request.payload.lastname);
+});
+
+test('Verify that the user is able to delete an existing booking using DELETE API: using Basic Authentication', {
+    tag: ['@API', '@SMOKE'],
+    annotation: {
+        type: "HTTP Method",
+        description: "DELETE call using Basic Authentication"
+    }
+}, async({ request, commonApiUtilsFixture })=>{
+    const authToken = await commonApiUtilsFixture.createToken();
+    expect(authToken).toMatch(/\S+/); //any non-whitespace character
+    //update token value in apiTestData
+    apiTestData.deleteBooking.request.resource = apiTestData.deleteBooking.request.resource.replace("{{ID}}", createdBookingID);
+    apiTestData.deleteBooking.request.headers.Cookie = apiTestData.deleteBooking.request.headers.Cookie.replace("{{authToken}}", authToken);
+    
+    const deleteBookingResponse = await request.delete(apiTestData.deleteBooking.request.resource, {
+        headers: apiTestData.deleteBooking.request.headers
+    });
+    //verify response
+    expect(deleteBookingResponse.status()).toBe(apiTestData.deleteBooking.response.statusCode);
+    expect(deleteBookingResponse.statusText()).toBe(apiTestData.deleteBooking.response.statusText);
+
+    // verify deletion
+    const getBookingResponse = await request.get(`${apiTestData.GET.request.resource}/${createdBookingID}`);
+    //verify response code/text
+    expect(getBookingResponse.status()).toBe(apiTestData.verifyDeletion.statusCode);
+    expect(getBookingResponse.statusText()).toBe(apiTestData.verifyDeletion.statusText);
 });
